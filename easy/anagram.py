fde3f9cb57ce884fff26cc4e720a81091ba454c8
shubham15s <shubham2031@gmail.com>
2024-10-25 17:10:30 +0530

    anagram and rotate_string

---
+++ easy/anagram.py
@@ -0,0 +1,46 @@
+'''
+Given two strings s and t, return true if t is an 
+anagram
+ of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+'''
+
+def isAnagram( s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    count = {}
+    for char in s:
+        count[char] = count.get(char, 0) + 1
+    for char in t:
+        if char in count:
+            count[char] -= 1
+            if count[char] < 0:
+                return False
+        else:
+            return False
+    return True
+
+s = "anagram"
+t = "nagaram"
+
+print(isAnagram(s, t))
